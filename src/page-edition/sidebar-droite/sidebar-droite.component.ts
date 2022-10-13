ad871b6674b7234ce27f3c5a09557539735ac627
Alex Godfrin <neptuna1998@gmail.com>
2022-10-13 17:48:23 +0200

    modif sidebarDroite (select > list)

--- src/page-edition/sidebar-droite/sidebar-droite.component.ts
+++ src/page-edition/sidebar-droite/sidebar-droite.component.ts
@@ -9,7 +9,20 @@ export class SidebarDroiteComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  test() {
+    alert("TODO : Sauvegarder les modifs")
   }
+
+  saveChanges(){
+    alert("TODO : Sauvegarder en JSON les modifications");
+  }
+
+  loadLabelInformations(e : any) {
+    var label : any = e.target;
+    alert("TODO : Récupérer et charger les infos du label" + label.id + " (titre, position, taille)");
+  }
+
 
 }
