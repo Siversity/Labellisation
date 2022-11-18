34b2f73ada7afc6d3598f3d2e7c9c439c2300bc0
Stéphane <81stephane@gmail.com>
2022-11-18 16:52:13 +0100

    Début backend

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const port = 4200;
+const path = require('path');
+
+app.use(express.static('dist/labellisation/'));
+app.use(express.static('image'));
+
+
+app.listen(port, () => {
+  console.log('My Demo App listening on ${port} port')
+});
+
+//#region 
+
+//#endregion
+
+app.get('/', function (req, res) {
+  res.sendFile(path.resolve('dist/labellisation/index.html'));
+});
+
+app.get('/testJSON', function (req, res) {
+  res.json({
+    number: 1,
+    name: 'John',
+    gender: 'male'
+  });
+});
