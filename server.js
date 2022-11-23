977bcfce77a728e3e17f73bf7f291affc6626904
Alex Godfrin <neptuna1998@gmail.com>
2022-11-23 08:19:58 +0100

    ajout region server.js

--- server.js
+++ server.js
@@ -1,56 +1,59 @@
-const express = require('express');
-const app = express();
-const port = 4200;
-const path = require('path');
-var fs = require('fs');
+//#region INIT
+  const express = require('express');
+  const app = express();
+  const port = 4200;
+  const path = require('path');
+  var fs = require('fs');
 
-app.use(express.static('dist/labellisation/'));
-app.use(express.static('image'));
-app.use(express.json())
+  app.use(express.static('dist/labellisation/'));
+  app.use(express.static('image'));
+  app.use(express.json())
 
 
-app.listen(port, () => {
-  console.log('My Demo App listening on port '+ port)
-});
-
-//#region 
-
+  app.listen(port, () => {
+    console.log('My Demo App listening on port '+ port)
+  });
 //#endregion
 
-app.get('/', function (req, res) {
-  res.sendFile(path.resolve('dist/labellisation/index.html'));
-});
+//#region GETTER
+  app.get('/', function (req, res) {
+    res.sendFile(path.resolve('dist/labellisation/index.html'));
+  });
 
-app.get('/imagesNameList', function (req, res) {
-  const json = { list : [] };
-  fs.readdir('image', function(error, files) {
-    var toPut = [];
-    files.forEach(function (file, index) {
-      toPut.push({"name" : file})
+  app.get('/imagesNameList', function (req, res) {
+    const json = { list : [] };
+    fs.readdir('image', function(error, files) {
+      var toPut = [];
+      files.forEach(function (file, index) {
+        toPut.push({"name" : file})
+      })
+      json.list = toPut;
+      res.json(json)
     })
-    json.list = toPut;
-    res.json(json)
   })
-})
 
-app.get('/testJSON', function (req, res) {
-  res.json({
-    number: 1,
-    name: 'John',
-    gender: 'male'
+  app.get('/testJSON', function (req, res) {
+    res.json({
+      number: 1,
+      name: 'John',
+      gender: 'male'
+    });
   });
-});
+//#endregion
 
-app.post('/', function(req, res){
-  var dict = {
-    box : req.body.box,
-    class : req.body.class,
-    text : req.body.text
-  }
-  var dictstring = JSON.stringify(dict);
+//#region POST
+  app.post('/', function(req, res){
+    var dict = {
+      box : req.body.box,
+      class : req.body.class,
+      text : req.body.text
+    }
+    var dictstring = JSON.stringify(dict);
 
-  fs.writeFile("src/assets/jsons/" + req.body.filename+".json", dictstring, function(err, result) {
-    if(err) console.log('error', err);
+    fs.writeFile("src/assets/jsons/" + req.body.filename+".json", dictstring, function(err, result) {
+      if(err) console.log('error', err);
+    });
+    res.send("it worked")
   });
-  res.send("it worked")
-});
+//#endregion
+
