f0eaa5f197c08ab6857a8fcbb067999a8ba697af
unknown <alex.godfrin@etud.univ-jfc.fr>
2022-11-25 10:00:07 +0100

    création liste-image

--- src/app/page-edition/page-edition.module.ts
+++ src/app/page-edition/page-edition.module.ts
@@ -5,6 +5,7 @@ import { PageEditionRoutingModule } from './page-edition-routing.module';
 import { PageEditionComponent } from './page-edition.component';
 import { SidebarDroiteComponent } from './sidebar-droite/sidebar-droite.component';
 import { SidebarGaucheComponent } from './sidebar-gauche/sidebar-gauche.component';
+import { ListeImageComponent } from './liste-image/liste-image.component';
 import { ImageComponent } from './image/image.component';
 
 @NgModule({
@@ -12,7 +13,8 @@ import { ImageComponent } from './image/image.component';
     PageEditionComponent,
     SidebarGaucheComponent,
     SidebarDroiteComponent,
-    ImageComponent
+    ImageComponent,
+    ListeImageComponent
   ],
   imports: [
     BrowserModule,
